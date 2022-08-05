7acaaf29b473769a9abeff761ff9f886e54adbed
Konstanty <Kostu96@gmail.com>
2022-08-05 16:12:57 +0200

    WIP

--- assets/scripts/PlayerController.lua
+++ assets/scripts/PlayerController.lua
@@ -1,13 +1,20 @@
 PlayerController = LuaScript:new()
 
-PlayerController.Speed = 30
+PlayerController.Speed = 50
 
 function PlayerController:onCreate()
     self.rigidbody = self:getComponent(Components.Rigidbody2D)
 end
 
 function PlayerController:onUpdate(dt)
+    local xVel = 0
+    if (Input.isKeyPressed(Input.Key.A)) then
+        xVel = -self.Speed
+    elseif (Input.isKeyPressed(Input.Key.D)) then
+        xVel = self.Speed
+    end
 
+    self.rigidbody:setVelocity(xVel * dt, 0)
 end
 
 return PlayerController
