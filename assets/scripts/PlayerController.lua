62489716af8a122d8c860229d2cf841a60302a60
Konstanty <Kostu96@gmail.com>
2022-08-05 10:26:52 +0200

    WIP

--- assets/scripts/PlayerController.lua
+++ assets/scripts/PlayerController.lua
@@ -1,9 +1,9 @@
-PlayerController = {}
+PlayerController = LuaScript:new()
 
 PlayerController.Speed = 30
 
 function PlayerController:onCreate()
-    
+    self.rigidbody = self:getComponent(Components.Rigidbody2D)
 end
 
 function PlayerController:onUpdate(dt)
