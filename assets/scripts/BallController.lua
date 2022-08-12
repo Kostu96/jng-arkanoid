073a213303a9ad74ac00502837fb93ef2cfac2ef
Konstanty <Kostu96@gmail.com>
2022-08-12 20:27:23 +0200

    WIP

--- assets/scripts/BallController.lua
+++ assets/scripts/BallController.lua
@@ -2,19 +2,19 @@ BallController = LuaScript:create()
 
 BallController.Speed = 10
 
+local function respawn(ballController)
+    local angle = math.random(15, 180 - 15)
+    local xVel = math.cos(math.rad(angle)) * ballController.Speed
+    local yVel = math.sin(math.rad(angle)) * ballController.Speed
+    ballController.rigidbody:setLinearVelocity(xVel, yVel)
+end
+
 function BallController:onCreate()
     math.randomseed(os.time())
 
     self.rigidbody = self:getComponent(Component.Rigidbody2D)
 
-    self:respawn()
-end
-
-function BallController:respawn()
-    local angle = math.random(15, 180 - 15)
-    local xVel = math.cos(math.rad(angle)) * self.Speed
-    local yVel = math.sin(math.rad(angle)) * self.Speed
-    self.rigidbody:setLinearVelocity(xVel, yVel)
+    respawn(self)
 end
 
 return BallController
--- assets/scripts/BlockSpawner.lua
+++ assets/scripts/BlockSpawner.lua
@@ -1,7 +1,14 @@
 BlockSpawner = LuaScript:create()
 
+BlockSpawner.Columns = 8
+BlockSpawner.Rows = 5
+
 function BlockSpawner:onCreate()
-    
+    for c = 0, self.Columns - 1 do
+        for r = 0, self.Rows - 1 do
+            local block = self:createEntity("Block" .. c .. r)
+        end
+    end
 end
 
 return BlockSpawner
